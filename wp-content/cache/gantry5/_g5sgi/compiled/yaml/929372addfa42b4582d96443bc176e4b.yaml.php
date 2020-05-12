9d9d1364b8343830c1dc38b68c9fc55c34bba0dd
Jess <jess@spirographics.com>
2020-05-12 17:07:13 -0500

    add style
    add style

--- wp-content/cache/gantry5/_g5sgi/compiled/yaml/929372addfa42b4582d96443bc176e4b.yaml.php
+++ wp-content/cache/gantry5/_g5sgi/compiled/yaml/929372addfa42b4582d96443bc176e4b.yaml.php
@@ -2,7 +2,7 @@
 return [
     '@class' => 'Gantry\\Component\\File\\CompiledYamlFile',
     'filename' => '/Applications/MAMP/htdocs/spiro_base_wp/wp-content/themes/_g5sgi/gantry/presets.yaml',
-    'modified' => 1589317615,
+    'modified' => 1589320995,
     'data' => [
         'preset1' => [
             'image' => 'gantry-admin://images/preset1.png',
@@ -76,357 +76,70 @@ return [
             ]
         ],
         'preset2' => [
-            'image' => 'gantry-admin://images/preset2.png',
-            'description' => 'GANTRY5_PLATFORM_PRESET2',
+            'description' => 'Style 2 from Jes',
             'colors' => [
-                0 => '#b34b4b',
-                1 => '#4cad9e'
+                0 => '#b8dfff',
+                1 => '#e3d98d'
             ],
             'styles' => [
-                'base' => [
-                    'background' => '#ffffff',
-                    'text-color' => '#424753'
-                ],
-                'accent' => [
-                    'color-1' => '#b34b4b',
-                    'color-2' => '#4cad9e'
-                ],
-                'link' => [
-                    'regular' => '#b34b4b',
-                    'hover' => '#424753'
-                ],
-                'font' => [
-                    'family-default' => 'Raleway, Helvetica, Tahoma, Geneva, Arial, sans-serif',
-                    'family-title' => 'Lato, Helvetica, Tahoma, Geneva, Arial, sans-serif'
-                ],
-                'navigation' => [
-                    'background' => '#312f38',
-                    'text-color' => '#ffffff'
-                ],
                 'header' => [
-                    'background' => '#312f38',
-                    'background-image' => 'gantry-media://header/img01.jpg',
-                    'background-overlay' => 'enabled',
-                    'text-color' => '#eceeef'
-                ],
-                'intro' => [
-                    'background' => '#f4f5f7',
-                    'text-color' => '#424753'
-                ],
-                'features' => [
-                    'background' => '#ffffff',
-                    'text-color' => '#424753'
-                ],
-                'utility' => [
-                    'background' => '#424753',
+                    'background' => '#7a7a7a',
                     'text-color' => '#ffffff'
                 ],
-                'above' => [
-                    'background' => '#f4f5f7',
-                    'text-color' => '#424753'
+                'showcase' => [
+                    'background' => '#3d3d3d',
+                    'image' => '',
+                    'text-color' => '#ffffff'
                 ],
-                'testimonials' => [
-                    'background' => '#b34b4b',
-                    'background-image' => 'gantry-media://testimonials/img01.jpg',
-                    'background-overlay' => 'enabled',
-                    'text-color' => '#eceeef'
+                'subfeature' => [
+                    'background' => '#f0f0f0',
+                    'text-color' => '#666666'
                 ],
-                'expanded' => [
+                'feature' => [
                     'background' => '#ffffff',
-                    'text-color' => '#424753'
+                    'text-color' => '#666666'
                 ],
                 'footer' => [
-                    'background' => '#312f38',
-                    'text-color' => '#ffffff'
-                ],
-                'offcanvas' => [
-                    'background' => '#b34b4b',
-                    'text-color' => '#eceeef',
-                    'toggle-color' => '#b34b4b'
-                ]
-            ]
-        ],
-        'preset3' => [
-            'image' => 'gantry-admin://images/preset3.png',
-            'description' => 'GANTRY5_PLATFORM_PRESET3',
-            'colors' => [
-                0 => '#c06c84',
-                1 => '#355c7d'
-            ],
-            'styles' => [
-                'base' => [
                     'background' => '#ffffff',
-                    'text-color' => '#424753'
+                    'text-color' => '#666666'
                 ],
-                'accent' => [
-                    'color-1' => '#c06c84',
-                    'color-2' => '#355c7d'
-                ],
-                'link' => [
-                    'regular' => '#c06c84',
-                    'hover' => '#424753'
-                ],
-                'font' => [
-                    'family-default' => 'Raleway, Helvetica, Tahoma, Geneva, Arial, sans-serif',
-                    'family-title' => 'Lato, Helvetica, Tahoma, Geneva, Arial, sans-serif'
+                'main' => [
+                    'background' => '#ffffff',
+                    'text-color' => '#666666'
                 ],
                 'navigation' => [
-                    'background' => '#312f38',
-                    'text-color' => '#ffffff'
-                ],
-                'header' => [
-                    'background' => '#355c7d',
-                    'background-image' => 'gantry-media://header/img01.jpg',
-                    'background-overlay' => 'enabled',
-                    'text-color' => '#eceeef'
-                ],
-                'intro' => [
-                    'background' => '#f4f5f7',
-                    'text-color' => '#424753'
-                ],
-                'features' => [
-                    'background' => '#ffffff',
-                    'text-color' => '#424753'
-                ],
-                'utility' => [
-                    'background' => '#355c7d',
-                    'text-color' => '#ffffff'
-                ],
-                'above' => [
-                    'background' => '#f4f5f7',
-                    'text-color' => '#424753'
-                ],
-                'testimonials' => [
-                    'background' => '#c06c84',
-                    'background-image' => 'gantry-media://testimonials/img01.jpg',
-                    'background-overlay' => 'enabled',
-                    'text-color' => '#eceeef'
-                ],
-                'expanded' => [
-                    'background' => '#ffffff',
-                    'text-color' => '#424753'
-                ],
-                'footer' => [
-                    'background' => '#355c7d',
-                    'text-color' => '#ffffff'
+                    'background' => '#5c5c5c',
+                    'text-color' => '#ffffff',
+                    'overlay' => 'rgba(0, 0, 0, 0.4)'
                 ],
                 'offcanvas' => [
-                    'background' => '#355c7d',
-                    'text-color' => '#eceeef',
-                    'toggle-color' => '#c06c84'
-                ]
-            ]
-        ],
-        'preset4' => [
-            'image' => 'gantry-admin://images/preset4.png',
-            'description' => 'GANTRY5_PLATFORM_PRESET4',
-            'colors' => [
-                0 => '#e84a5f',
-                1 => '#2a363b'
-            ],
-            'styles' => [
+                    'background' => '#354d59',
+                    'text-color' => '#ffffff',
+                    'width' => '17rem',
+                    'toggle-color' => '#ffffff',
+                    'toggle-visibility' => '1'
+                ],
                 'base' => [
                     'background' => '#ffffff',
-                    'text-color' => '#2a363b'
+                    'text-color' => '#666666',
+                    'body-font' => 'roboto, sans-serif',
+                    'heading-font' => 'roboto, sans-serif'
                 ],
                 'accent' => [
-                    'color-1' => '#e84a5f',
-                    'color-2' => '#2a363b'
+                    'color-1' => '#b8dfff',
+                    'color-2' => '#e3d98d'
                 ],
-                'link' => [
-                    'regular' => '#e84a5f',
-                    'hover' => '#2a363b'
+                'breakpoints' => [
+                    'large-desktop-container' => '75rem',
+                    'desktop-container' => '60rem',
+                    'tablet-container' => '48rem',
+                    'large-mobile-container' => '30rem',
+                    'mobile-menu-breakpoint' => '48rem'
                 ],
-                'font' => [
-                    'family-default' => 'Raleway, Helvetica, Tahoma, Geneva, Arial, sans-serif',
-                    'family-title' => 'Lato, Helvetica, Tahoma, Geneva, Arial, sans-serif'
-                ],
-                'navigation' => [
-                    'background' => '#2a363b',
-                    'text-color' => '#ffffff'
-                ],
-                'header' => [
-                    'background' => '#2a363b',
-                    'background-image' => 'gantry-media://header/img01.jpg',
-                    'background-overlay' => 'enabled',
-                    'text-color' => '#eceeef'
-                ],
-                'intro' => [
-                    'background' => '#f4f5f7',
-                    'text-color' => '#2a363b'
-                ],
-                'features' => [
-                    'background' => '#ffffff',
-                    'text-color' => '#2a363b'
-                ],
-                'utility' => [
-                    'background' => '#2a363b',
-                    'text-color' => '#ffffff'
-                ],
-                'above' => [
-                    'background' => '#f4f5f7',
-                    'text-color' => '#2a363b'
-                ],
-                'testimonials' => [
-                    'background' => '#e84a5f',
-                    'background-image' => 'gantry-media://testimonials/img01.jpg',
-                    'background-overlay' => 'enabled',
-                    'text-color' => '#eceeef'
-                ],
-                'expanded' => [
-                    'background' => '#ffffff',
-                    'text-color' => '#2a363b'
-                ],
-                'footer' => [
-                    'background' => '#2a363b',
-                    'text-color' => '#ffffff'
-                ],
-                'offcanvas' => [
-                    'background' => '#2a363b',
-                    'text-color' => '#eceeef',
-                    'toggle-color' => '#e84a5f'
-                ]
-            ]
-        ],
-        'preset5' => [
-            'image' => 'gantry-admin://images/preset5.png',
-            'description' => 'GANTRY5_PLATFORM_PRESET5',
-            'colors' => [
-                0 => '#a8a7a7',
-                1 => '#474747'
-            ],
-            'styles' => [
-                'base' => [
-                    'background' => '#ffffff',
-                    'text-color' => '#2a363b'
-                ],
-                'accent' => [
-                    'color-1' => '#a8a7a7',
-                    'color-2' => '#474747'
-                ],
-                'link' => [
-                    'regular' => '#a8a7a7',
-                    'hover' => '#2a363b'
-                ],
-                'font' => [
-                    'family-default' => 'Raleway, Helvetica, Tahoma, Geneva, Arial, sans-serif',
-                    'family-title' => 'Lato, Helvetica, Tahoma, Geneva, Arial, sans-serif'
-                ],
-                'navigation' => [
-                    'background' => '#474747',
-                    'text-color' => '#ffffff'
-                ],
-                'header' => [
-                    'background' => '#474747',
-                    'background-image' => 'gantry-media://header/img01.jpg',
-                    'background-overlay' => 'enabled',
-                    'text-color' => '#eceeef'
-                ],
-                'intro' => [
-                    'background' => '#f4f5f7',
-                    'text-color' => '#2a363b'
-                ],
-                'features' => [
-                    'background' => '#ffffff',
-                    'text-color' => '#2a363b'
-                ],
-                'utility' => [
-                    'background' => '#474747',
-                    'text-color' => '#ffffff'
-                ],
-                'above' => [
-                    'background' => '#f4f5f7',
-                    'text-color' => '#2a363b'
-                ],
-                'testimonials' => [
-                    'background' => '#a8a7a7',
-                    'background-image' => 'gantry-media://testimonials/img01.jpg',
-                    'background-overlay' => 'enabled',
-                    'text-color' => '#eceeef'
-                ],
-                'expanded' => [
-                    'background' => '#ffffff',
-                    'text-color' => '#2a363b'
-                ],
-                'footer' => [
-                    'background' => '#474747',
-                    'text-color' => '#ffffff'
-                ],
-                'offcanvas' => [
-                    'background' => '#474747',
-                    'text-color' => '#eceeef',
-                    'toggle-color' => '#a8a7a7'
-                ]
-            ]
-        ],
-        'preset6' => [
-            'image' => 'gantry-admin://images/preset6.png',
-            'description' => 'GANTRY5_PLATFORM_PRESET6',
-            'colors' => [
-                0 => '#474747',
-                1 => '#363636'
-            ],
-            'styles' => [
-                'base' => [
-                    'background' => '#363636',
-                    'text-color' => '#a8a7a7'
-                ],
-                'accent' => [
-                    'color-1' => '#a8a7a7',
-                    'color-2' => '#474747'
-                ],
-                'link' => [
-                    'regular' => '#a8a7a7',
-                    'hover' => '#eceeef'
-                ],
-                'font' => [
-                    'family-default' => 'Raleway, Helvetica, Tahoma, Geneva, Arial, sans-serif',
-                    'family-title' => 'Lato, Helvetica, Tahoma, Geneva, Arial, sans-serif'
-                ],
-                'navigation' => [
-                    'background' => '#474747',
-                    'text-color' => '#ffffff'
-                ],
-                'header' => [
-                    'background' => '#474747',
-                    'background-image' => 'gantry-media://header/img01.jpg',
-                    'background-overlay' => 'enabled',
-                    'text-color' => '#eceeef'
-                ],
-                'intro' => [
-                    'background' => '#474747',
-                    'text-color' => '#ffffff'
-                ],
-                'features' => [
-                    'background' => '#363636',
-                    'text-color' => '#ffffff'
-                ],
-                'utility' => [
-                    'background' => '#474747',
-                    'text-color' => '#ffffff'
-                ],
-                'above' => [
-                    'background' => '#a8a7a7',
-                    'text-color' => '#ffffff'
-                ],
-                'testimonials' => [
-                    'background' => '#a8a7a7',
-                    'background-image' => 'gantry-media://testimonials/img01.jpg',
-                    'background-overlay' => 'enabled',
-                    'text-color' => '#eceeef'
-                ],
-                'expanded' => [
-                    'background' => '#363636',
-                    'text-color' => '#ffffff'
-                ],
-                'footer' => [
-                    'background' => '#474747',
-                    'text-color' => '#ffffff'
-                ],
-                'offcanvas' => [
-                    'background' => '#474747',
-                    'text-color' => '#eceeef',
-                    'toggle-color' => '#a8a7a7'
+                'menu' => [
+                    'col-width' => '180px',
+                    'animation' => 'g-fade',
+                    'hide-on-mobile' => '0'
                 ]
             ]
         ]
