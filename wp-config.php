78a8fca9c1b0c1258b2a58e36a0060ad15a98dbe
Jess Erickson <615jess@gmail.com>
2020-05-13 23:58:52 -0500

    Fixed Spiro Cards
    fixed Spiro Card

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'spiro-wp-gantry' );
+define( 'DB_NAME', 'spiro-wp' );
 
 /** MySQL database username */
 define( 'DB_USER', 'root' );
@@ -77,7 +77,7 @@ $table_prefix = 'wp_';
  *
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', false );
+define( 'WP_DEBUG', true );
 
 /* That's all, stop editing! Happy publishing. */
 
